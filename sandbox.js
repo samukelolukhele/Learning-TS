6e1d4d42ceb3ec7a46ba5922290439b2953ef2eb
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-18 11:29:12 +0200

    Learned how types work

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,24 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('sean');
+// numbers[1] = false
+var mixed = ['ken', 4, 'chun-li', 8, 9, true];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '45'
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    age: 30,
+    name: 'yoshi',
+    belt: 'orange'
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,33 @@
-const character = 'mario';
 
-console.log(character);
+let names = ['luigi', 'mario', 'yoshi'];
 
-const inputs = document.querySelectorAll('input');
+names.push('toad');
 
-inputs.forEach(input => {
-  console.log(input);
-});
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push('sean');
+// numbers[1] = false
+
+let mixed = ['ken', 4, 'chun-li', 8, 9, true];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+let ninja = {
+ name: 'mario',
+ belt: 'black',
+ age: 30
+}
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '45'
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+  age: 30,
+  name: 'yoshi',
+  belt: 'orange'
+}
