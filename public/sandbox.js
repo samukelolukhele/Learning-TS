0e380fe06ac1ee305a008d2f5ed5a5584b1c24ef
Samukel Lukhele <kelo.lukhele@gmail.com>
2022-03-18 12:40:36 +0200

    Learned type aliases

---
+++ public/sandbox.js
@@ -0,0 +1,7 @@
+"use strict";
+const logDetails = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
+};
+const greet = (user) => {
+    console.log(`${user.name} has a uid of ${user.uid}`);
+};
---
+++ src/sandbox.ts
@@ -0,0 +1,10 @@
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum};
+
+const logDetails = (uid: StringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
+}
+
+const greet = (user: objWithName) => {
+    console.log(`${user.name} has a uid of ${user.uid}`);
+}
